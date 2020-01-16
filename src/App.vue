8b57fa62212767ec723a8db7fd776b34282eb81d
basakurt <basak.kurt@accenture.com>
2020-01-16 09:40:09 +0300

    first-commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">{{ message }}</div>
+</template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      message: "Hello Stranger!"
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-size: 40px;
+  color: #1aa7b9;
+}
+</style>
